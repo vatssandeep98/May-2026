ad61c0dd3603ce73e547525086e0868726d7a9e3
Sandeep Sharma <143292668+vatssandeep98@users.noreply.github.com>
2026-05-27 07:34:21 +0530

    Add new resource group 'rg22' to main.tf
    Added a new resource group 'rg22' for resource management.

--- 27-May/rg/main.tf
+++ 27-May/rg/main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg1" {
   name = "rg-2"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "rg22" {  #Ticket Number 0001 fo RG add
+  name = "rg-3"
+  location = "central india"
+}
